311d61190cc6ffc2cb105e21bd0c6b8dcaee9e27
Tshepo <rocboyt@gmail.com>
2021-01-03 10:52:47 +0200

    Pre card titles

--- Superhero/customControls/CardView.swift
+++ Superhero/customControls/CardView.swift
@@ -2,8 +2,7 @@ import UIKit
 
 @IBDesignable class CardView: UIView {
 
-    @IBInspectable var cornerRadius: CGFloat = 6
-
+    @IBInspectable var cornerRadius: CGFloat = 0
     @IBInspectable var shadowOffsetWidth: Int = 0
     @IBInspectable var shadowOffsetHeight: Int = 3
     @IBInspectable var shadowColor: UIColor? = hexStringToUIColor(hex:"#CCCCCC")
