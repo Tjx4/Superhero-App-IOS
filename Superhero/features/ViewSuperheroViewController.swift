065c2c384c164f96c8b57716c3c3abfa8f376aee
Tshepo <rocboyt@gmail.com>
2021-01-03 18:58:19 +0200

    Added outlets

--- Superhero/features/ViewSuperheroViewController.swift
+++ Superhero/features/ViewSuperheroViewController.swift
@@ -8,6 +8,17 @@ class ViewSuperheroViewController: UIViewController {
     @IBOutlet weak var txtSuperhero: UILabel!
     @IBOutlet weak var txtRealName: UILabel!
     @IBOutlet weak var txtPublisher: UILabel!
+    
+    @IBOutlet weak var lblInteligence: UILabel!
+    @IBOutlet weak var lblStrength: UILabel!
+    @IBOutlet weak var lblFullname: UILabel!
+    @IBOutlet weak var lblAlterEgo: UILabel!
+    @IBOutlet weak var lblPlaceOfBirth: UILabel!
+    @IBOutlet weak var lblApperance: UILabel!
+    @IBOutlet weak var lblHeight: UILabel!
+    @IBOutlet weak var lblWeight: UILabel!
+    
+    @IBOutlet weak var lblSpeed: UILabel!
     @IBOutlet weak var cvAppearance: CardView!
     @IBOutlet weak var navBar: UINavigationItem!
     @IBOutlet weak var imgvHeroImage: UIImageView!
