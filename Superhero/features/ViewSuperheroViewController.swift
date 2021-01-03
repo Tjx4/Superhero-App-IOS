5ea762af6bc4d11f10fcb6a4e001537818cef74d
Tshepo <rocboyt@gmail.com>
2021-01-03 12:19:19 +0200

    Added hero name

--- Superhero/features/ViewSuperheroViewController.swift
+++ Superhero/features/ViewSuperheroViewController.swift
@@ -3,16 +3,22 @@ import SDWebImage
 
 class ViewSuperheroViewController: UIViewController {
     
+    @IBOutlet weak var txtSuperhero: UILabel!
+    @IBOutlet weak var txtRealName: UILabel!
+    @IBOutlet weak var txtPublisher: UILabel!
+    
     @IBOutlet weak var navBar: UINavigationItem!
     @IBOutlet weak var imgvHeroImage: UIImageView!
     var superhero: Superhero?
     
     override func viewDidLoad() {
-        navBar.title = superhero?.name
+        txtSuperhero.text = superhero?.name
         
         let url = superhero?.image?.url ?? ""
         imgvHeroImage.sd_setImage(with: URL(string: url), placeholderImage: UIImage(named: "placeholder.png"))
         
     }
     
+    @IBAction func onMoreInfoClicked(_ sender: Any) {
+    }
 }
